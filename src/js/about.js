886662e22480bec0dd9e09ad2748a13e33cf991d
Vasyl Saliuk <67233665+VasylSaliuk@users.noreply.github.com>
2020-11-29 13:46:06 +0200

    add section About

--- src/js/about.js
+++ src/js/about.js
@@ -1,1 +1,43 @@
 import "../css/about.css";
+import myAvtoPark from "../data/items.js";
+// console.log(myAvtoPark);
+import aboutRefs from "./refs.js";
+const { aboutSection } = aboutRefs;
+// console.log(aboutSection);
+
+function createCarsList(arr) {
+  //   console.log(arr);
+  return arr.map((car) => {
+    const { name, picture, price } = car;
+    // console.log(name);
+    // console.log(picture);
+    const imgName = document.createElement("h3");
+    imgName.textContent = name;
+    const imgWrapper = document.createElement("div");
+    const pictures = picture.map((pic) => {
+      const img = document.createElement("img");
+      img.setAttribute("src", pic);
+      img.setAttribute("alt", name);
+      img.setAttribute("width", "200px");
+      //   console.log(img);
+      return img;
+    });
+    imgWrapper.append(...pictures);
+    // console.log(imgWrapper);
+    // console.log(price);
+    const carPrice = document.createElement("p");
+    carPrice.textContent = price;
+
+    const item = document.createElement("li");
+    item.append(imgName, imgWrapper, carPrice);
+    return item;
+  });
+}
+
+const carsItems = createCarsList(myAvtoPark);
+console.log(carsItems);
+
+const carsList = document.createElement("ul");
+carsList.append(...carsItems);
+// console.log(carsList);
+aboutSection.append(carsList);
--- src/js/home.js
+++ src/js/home.js
@@ -1,1 +1,19 @@
 import "../css/home.css";
+import refs from "./refs.js";
+console.log(refs);
+console.log(refs.homeSection);
+// Деструктуризация обекта доступа
+const { homeSection } = refs;
+console.log(homeSection);
+
+const homeTitle = `<h2 class="homeTitle">Home</h2>`;
+
+homeSection.insertAdjacentHTML("afterbegin", homeTitle);
+
+const homeContent = document.createElement("p");
+const content = `Lorem ipsum, dolor sit amet consecteture. Harum ipsum et non odit quis nemo reiciendis consectetur natus perspiciatis, officia, exercitationem nostrum voluptas debitis dignissimos, esse delectus.`;
+homeContent.textContent = content;
+console.log(homeContent);
+
+const title = document.querySelector(".homeTitle");
+title.appendChild(homeContent);
